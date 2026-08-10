756799dad33403db5e758ece3bc2bc7aa77c7f2c
Masahiro Takahashi <matakahashi0053@gmail.com>
2026-08-10 20:28:48 +0900

    Revert "品質向上: strength=0.6, steps=30, guidance=7.5"
    This reverts commit 30e942e242cc2161759c868979b4df29360a2662.

--- handler.py
+++ handler.py
@@ -111,9 +111,9 @@ def handler(job):
             video=input_frames,
             height=new_h,
             width=new_w,
-            strength=0.6,
-            num_inference_steps=30,
-            guidance_scale=7.5,
+            strength=0.75,
+            num_inference_steps=20,
+            guidance_scale=9.0,
             generator=torch.Generator("cuda").manual_seed(42),
         )
 
