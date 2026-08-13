ae4f2780820324c95b65b746f15c8132df90b564
Masahiro Takahashi <matakahashi0053@gmail.com>
2026-08-13 11:19:16 +0900

    強化版: strength=0.55

--- handler.py
+++ handler.py
@@ -111,7 +111,7 @@ def handler(job):
             video=input_frames,
             height=new_h,
             width=new_w,
-            strength=0.5,
+            strength=0.55,
             num_inference_steps=25,
             guidance_scale=7.0,
             generator=torch.Generator("cuda").manual_seed(42),
